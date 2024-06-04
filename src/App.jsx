b8994670a0f71425103f6a829437d190b479d3a4
James <apisitamornktn@gmail.com>
2024-06-04 16:26:27 +0700

    feat:React Rendering List Items Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import { movies } from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <h1 className="movie-list-header">Movie List Section</h1>
+        <div className="MovieSection">
+          {movies.map((item, index) => {
+            return (
+              <div className="Movie" key={index}>
+                <div>
+                  <img src={item.image}></img>
+                </div>
+                <div className="Info">
+                  <div>Title: {item.title}</div>
+                  <div>Year: {item.year}</div>
+                  <div>Runtime: {item.runtime}</div>
+                  <div className="Type">
+                    Genres:
+                    {item.genres.map((genre, i) => {
+                      return (
+                        <div key={i} className="TypeInfo">
+                          {genre}
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div>IMDB Ratings: {item.imdbRating}</div>
+                  <div>IMDB Votes: {item.imdbVotes}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
